ccbcb110f0329d4b7ac3b56c3d75f1a8428b5e58
Mauricio Mendez <emauriciomendez@gmail.com>
2023-05-02 01:27:07 -0500

    componente Detail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingHome from "./components/LandingHome/LandingHome";
 import About from "./components/About/About";
+import Detail from "./components/Detail/Detail";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingHome />} />
           <Route path="/about" element={<About />} />
+          <Route path="/detail" element={<Detail />} />
 
         </Routes>
       </header>
