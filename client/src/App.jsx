d9eaf7240eb98a1379fdc2c26884c77b541b66c0
Mauricio Mendez <emauriciomendez@gmail.com>
2023-05-01 21:08:19 -0500

    cambio about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Landing from "./components/Landing/Landing.jsx";
-import Detail from "./components/Detail/Detail.jsx";
-import About from "./components/About/About.jsx";
+import Landing from "./components/Landing/Landing";
 
 function App() {
   return (
@@ -10,8 +8,6 @@ function App() {
       <header className="App-header">
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/detail" element={<Detail />} />
-          <Route path="/about" element={<About />} />
         </Routes>
       </header>
     </div>
--- client/src/components/About/About.jsx
+++
@@ -1,7 +0,0 @@
-const about=()=>{
-
-    return 
-    <div className="container">
-<H1>ABOUT</H1>
-    </div>
-}
