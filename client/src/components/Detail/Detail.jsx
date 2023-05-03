81a341c53ec5113bcf1d946399fec4d297ad3336
Mauricio Mendez <emauriciomendez@gmail.com>
2023-05-02 22:43:34 -0500

    cambios detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -2,25 +2,26 @@
 import { useParams} from "react-router-dom";
 
 import style from './Detail.module.css'
+import NavBar from "../NavBar/NavBar";
 
    
 const Detail = ()=>{
     const { id } = useParams();   
-   const [product, setProduct] = React.useState({}); 
+   const [product1, setProduct] = React.useState({}); 
     console.log(id)
 //   //  01702cfd-2ec1-4c74-9439-519594fbc43d
-  // const product= {
-  //   name: "Leche de almendras",
-  //   image: "https://biofresh.shop/img/leche-almendras.jpg",//'../../assets/img/leche-almendras.jpg'
-  //   description:
-  //     "Leche vegetal de almendras, rica en grasas saludables y nutrientes.",
-  //   price: 3.5,
-  //   stock: 15,
-  // }
+  const product= {
+    name: "Leche de almendras",
+    image: "https://biofresh.shop/img/leche-almendras.jpg",//'../../assets/img/leche-almendras.jpg'
+    description:
+      "Leche vegetal de almendras, rica en grasas saludables y nutrientes.",
+    price: 3.5,
+    stock: 15,
+  }
  
   const getProductForId= async()=>{
     try {
-      const Data= await axios(`http://localhost:3001/products/${id}` )
+          const Data= await axios(`http://localhost:3001/products/${id}` )
           const char=Data.data;
           console.log(char)         
             if (char.length>=1) {   setProduct(char[0]);  }
@@ -38,23 +39,13 @@ const Detail = ()=>{
      }, [id]);
 
 
-//   const history = useHistory()
-//   function backToHome(){ history.push("/home")    }
-
-//   function format(num)  {    
-//       if(!isNaN(num)){
-//         num = num.toString().split("").reverse().join("").replace(/(?=\d*\.?)(\d{3})/g,'$1.');
-//         num = num.split("").reverse().join("").replace(/^[.]/, "");
-//         return num;      
-//       }  
-//     }
   
 return(
     <div className={style.container}>
        
-        
+        <NavBar/>
         <div className={style.container2}>
-        <div className={style.contInf} >
+        <div className={style.contInfTitle} >
         
             <h3 className={style.title}>{product.name}</h3> 
             <p> {product.description}</p>
@@ -64,14 +55,15 @@ return(
             <div className={style.price}><label>$ </label>{product.price}</div>
             <div><label>Disponibles: </label>{product.stock}</div>
             
-            <div><label>Cantidad:<input type="number" className={style.cantidad}   /> </label></div>
-            <button  className={style.btn}> Add to Car</button> 
+            <div><label>Cantidad:<input type="number" className={style.cantidad}  min={1} /> </label></div>
+            <button  className={style.btn}> Add to Cart 🛒</button> 
           
           </div>
             
            
         </div>
-        {/* <button onClick={backToHome} className={style.btn}> Volver</button> */}
+        <button  className={style.btn}>◁◁  Volver</button>
+        {/* onClick={backToHome} */}
     </div>
 )
 
