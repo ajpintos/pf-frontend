14b908853fb139d57256846c3da629caafec339d
Mario Benitez <nadamasfacil.mb@gmail.com>
2023-05-04 11:14:08 -0600

    step 1

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,19 +5,17 @@ const Footer = () => {
     <div className='col bg-secondary' >
       <div className='row' >
         <figure className='col-sm-6 col-md-3 col-lg-3'>
-          <img src={imgpropia} alt="Biofresh Logo" className='img-fluid w-50' />
+          <img src={imgpropia} alt="Biofresh Logo" className='img-fluid w-55 p-3' />
         </figure>
         <article className='col-sm-6 col-md-3 col-lg-3'>
-          <h4 className='text-warning-emphasis'>
-            INFORMATION
-          </h4>
-          <p className='h6 text-warning'>
+          <h4 className='text-warning-emphasis p-3'>INFORMATION</h4>
+          <p className='h6 text-warning p-3'>
           This web page is the result of the Final Project of Henry's FullStack Master.
           </p>
         </article>
         <article className='col-sm-6 col-md-3 col-lg-3'>
-          <h4 className='text-warning-emphasis'>Group 5 of cohort Pt-10b</h4>
-          <p className='h6 text-warning'>
+          <h4 className='text-warning-emphasis p-3'>Group 5 Pt-10b</h4>
+          <p className='h6 text-warning p-3'>
             Juan Cruz Matanzo <br/>
             Santiago Muller <br/>
             Javier Pintos <br/>
@@ -27,10 +25,8 @@ const Footer = () => {
           </p>
         </article>
         <article className='col-sm-6 col-md-3 col-lg-3'>
-          <h4 className='h5 text-warning-emphasis'>
-            MENU
-          </h4>
-          <ul className='h5 list-unstyled text-primary'>
+          <h4 className='h5 text-warning-emphasis p-3'>MENU</h4>
+          <ul className='h5 list-unstyled text-primary p-3'>
             <li >Home</li>
             <li>Categories</li>
             <li>About Us</li>
@@ -38,7 +34,7 @@ const Footer = () => {
           </ul>
         </article> 
       </div>
-      <h5 className='text-warning-emphasis'>Copyright 2023</h5>
+      <h5 className='text-warning-emphasis p-3'>Copyright 2023</h5>
     </div>
   )
 };
