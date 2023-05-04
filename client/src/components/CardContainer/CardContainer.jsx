792fad139450e89a5add5ed081f3fcc7db5a5f92
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-03 21:48:06 -0500

    detail 85% completado

--- client/src/components/CardContainer/CardContainer.jsx
+++ client/src/components/CardContainer/CardContainer.jsx
@@ -1,39 +1,39 @@
 import React from "react";
-import { useEffect , useState } from "react";
-import styles from "./CardContainer.module.css"
+import { useEffect, useState } from "react";
+import styles from "./CardContainer.module.css";
 import Card from "../Card/Card.jsx";
 import axios from "axios";
 
-function CardContainer () {
-
+function CardContainer() {
   const [products, setProducts] = useState([]);
 
   const callApi = async () => {
-    const api = await axios("/products")
+    const api = await axios("/products");
     const apiData = api.data;
     setProducts(apiData);
     return products;
-  }
+  };
 
   useEffect(() => {
-     callApi();
-  },[])
+    callApi();
+  }, []);
 
-    return (
-        <div className={styles.container_productos}>
-            {products.map(product => (
-                    <Card
-                        key={product.name}
-                        name={product.name}
-                        image={product.image}
-                        description={product.description}
-                        price={product.price}
-                        stock={product.stock} 
-                    />
-                ))
-            }
-        </div>
-    )
+  return (
+    <div className={styles.container_productos}>
+      {products.map((product) => (
+        
+        <Card
+          key={product.name}
+          id={product.id}
+          name={product.name}
+          image={product.image}
+          description={product.description}
+          price={product.price}
+          stock={product.stock}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default CardContainer;
--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,72 +1,80 @@
- import React from "react";
-import { useParams} from "react-router-dom";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import style from './Detail.module.css'
+
+import style from "./Detail.module.css";
 import NavBar from "../NavBar/NavBar";
 
-   
-const Detail = ()=>{
-    const { id } = useParams();   
-   const [product1, setProduct] = React.useState({}); 
-    console.log(id)
-//   //  01702cfd-2ec1-4c74-9439-519594fbc43d
-  const product= {
-    name: "Leche de almendras",
-    image: "https://biofresh.shop/img/leche-almendras.jpg",//'../../assets/img/leche-almendras.jpg'
-    description:
-      "Leche vegetal de almendras, rica en grasas saludables y nutrientes.",
-    price: 3.5,
-    stock: 15,
-  }
- 
-  const getProductForId= async()=>{
-    try {
-          const Data= await axios(`http://localhost:3001/products/${id}` )
-          const char=Data.data;
-          console.log(char)         
-            if (char.length>=1) {   setProduct(char[0]);  }
-           else { window.alert("No hay ningun Producto con ese ID ");  }
-    } catch (error) {
-        window.alert("Error en la busqueda del Producto ")
-    }
-          
-
-   }
-
-  React.useEffect(() => {         
-    getProductForId();
-     // eslint-disable-next-line react-hooks/exhaustive-deps
-     }, [id]);
-
-
+const Detail = () => {
+  const { id } = useParams();
+  const [product, setProduct] = React.useState({});
   
-return(
+
+  const getProductForId = async () => {
+    try {
+      const Data = await axios(`http://localhost:3001/products/${id}`);
+      const char = Data.data;
+      console.log(char);
+      if (char) {
+        setProduct(char);
+      }
+    } catch (error) {
+      window.alert(error.message);
+    }
+  };
+
+  React.useEffect(() => {
+    getProductForId();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+
+
+  const validacion = () => {};
+
+  return (
     <div className={style.container}>
-       
-        <NavBar/>
-        <div className={style.container2}>
-        <div className={style.contInfTitle} >
-        
-            <h3 className={style.title}>{product.name}</h3> 
-            <p> {product.description}</p>
-            </div>
-          <img src={product.image} alt={product.name} width="320" height="320"/>
-          <div className={style.contInf} >
-            <div className={style.price}><label>$ </label>{product.price}</div>
-            <div><label>Disponibles: </label>{product.stock}</div>
-            
-            <div><label>Cantidad:<input type="number" className={style.cantidad}  min={1} /> </label></div>
-            <button  className={style.btn}> Add to Cart 🛒</button> 
-          
-          </div>
-            
-           
+      <NavBar />
+      <div className={style.container2}>
+        <div className={style.contInfTitle}>
+          <h3 className={style.title}>{product.name}</h3>
+          <p> {product.description}</p>
         </div>
-        <button  className={style.btn}>◁◁  Volver</button>
-        {/* onClick={backToHome} */}
+
+        <img src={product.image} alt={product.name} width="320" height="320" />
+        <div className={style.contInf}>
+          <div className={style.price}>
+            <label>$ </label>
+            {product.price}
+          </div>
+          <div>
+            <label>Disponibles: </label>
+            {product.stock}
+          </div>
+
+          <div>
+            <label>
+              Cantidad:
+              <input
+                type="number"
+                className={style.cantidad}
+                min={1}
+                max={product.stock}
+                onChange={validacion}
+              />{" "}
+            </label>
+          </div>
+          <button className={style.btn}> Add to Cart 🛒</button>
+          <button>❤️</button>
+        </div>
+      </div>
+      <Link to="/">
+        <button className={style.btn}>◁◁ Volver</button>
+      </Link>
+      {/* onClick={backToHome} */}
     </div>
-)
-
-
-}
+  );
+};
 export default Detail;
