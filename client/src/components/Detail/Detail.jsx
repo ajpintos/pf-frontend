6f72e0e2b7d9e541b98842afa547bb9de733a94b
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-03 22:47:00 -0500

    responsi 20/100 Detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
 import axios from "axios";
-
 
 import style from "./Detail.module.css";
 import NavBar from "../NavBar/NavBar";
@@ -10,7 +8,7 @@ import NavBar from "../NavBar/NavBar";
 const Detail = () => {
   const { id } = useParams();
   const [product, setProduct] = React.useState({});
-  const [cant, setCant]= React.useState(1)
+  const [cant, setCant] = React.useState(1);
 
   const getProductForId = async () => {
     try {
@@ -30,35 +28,37 @@ const Detail = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
-
-
   const validacion = (e) => {
-    setCant(e.target.value)
+    setCant(e.target.value);
   };
 
   return (
     <div className={style.container}>
-      <NavBar />
-      <div className={style.container2}>
-        <div className={style.contInfTitle}>
-          <h3 className={style.title}>{product.name}</h3>
-          <p> {product.description}</p>
-        </div>
-
-        <img src={product.image} alt={product.name} width="320" height="320" />
-        <div className={style.contInf}>
-          <div className={style.price}>
-            <label>$ </label>
-            {product.price}
-          </div>
-          <div>
-            <label>Disponibles: </label>
-            {product.stock}
+      <div class="container">
+        <NavBar />
+      </div>
+      <div class="border border-danger container-fluid">
+        <div class="row">
+          <div class="col">
+            <img
+              src={product.image}
+              alt={product.name}
+              width="320"
+              height="320"
+            />
           </div>
 
-          <div>
-            <label>
-              Cantidad:
+          <div class="col-md">
+            <h1 className={style.title}>{product.name}</h1>
+
+            <h2 className={style.price}>${product.price}</h2>
+            <p> {product.description}</p>
+            <div>
+              Disponibles:
+              {product.stock}
+            </div>
+
+            <div>
               <input
                 type="number"
                 className={style.cantidad}
@@ -66,17 +66,20 @@ const Detail = () => {
                 min={1}
                 max={product.stock}
                 onChange={validacion}
-              />{" "}
-            </label>
+              />
+              <button className={style.btn}> Add to Cart 🛒</button>
+            </div>
+            <br />
+            <button>❤️</button>
           </div>
-          <button className={style.btn}> Add to Cart 🛒</button>
-          <button>❤️</button>
         </div>
       </div>
-      <Link to="/">
-        <button className={style.btn}>◁◁ Volver</button>
-      </Link>
-      {/* onClick={backToHome} */}
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+        crossorigin="anonymous"
+      ></link>
     </div>
   );
 };
