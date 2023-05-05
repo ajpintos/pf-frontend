35fceffeb50a36c1dea50204d8c23677e9b63ef8
ajpintos <ajpintos@gmail.com>
2023-05-05 15:40:39 -0300

    login page almost ready (design 90% and functionalities still in progress)

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './LoginPage.module.css';
 import Footer from '../Footer/Footer.jsx';
+import NavBar from "../NavBar/NavBar.jsx";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import validate from "../RegisterPage/validate/validate.js";
 
-function LoginPage(props) {
-    return (<>
-        <div className={styles.container}>
-            <div className={styles.box}>
-                <h2>Login</h2>
-                <form>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Email"/>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Password"/>
-                </form>
-            </div>
-        </div>
-        <Footer/>
-    </>);
+function LoginPage(props){
 
+//! Estado local para guardar los datos del formulario
+const [form, setForm] = useState({
+    email: "",
+    password: "",
+})
+
+//! Estado local para guardar los errores de validación del formulario
+const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+})
+
+const submitHandler = (event) => {
+    event.preventDefault()
+    const response = axios.get("/users", form)
+    console.log("Este es el contenido del form", form)
+        .then(res => alert("User logged successfully!")
+            .catch(err => alert(err)))
 }
 
+
+const changeHandler = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
+    //! Elimina id delay de la validación
+    setForm({...form, [property]: value});
+    setErrors(validate({...form, [property]: value}));
+}
+
+return (<>
+        <NavBar/>
+        <div className={styles.formContainer}>
+            <Form onSubmit={submitHandler}>
+                <h2>Login</h2>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" id="email" name="email"
+                                      value={form.email}
+                                      onChange={changeHandler}/>
+                    </Form.Group>
+                    <br/>
+
+                    <Form.Group as={Col} controlId="formPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Password" id="password" name="password"
+                                      value={form.password}
+                                      onChange={changeHandler}/>
+                    </Form.Group>
+                </Row>
+                <br/>
+                <Button variant="success" type="submit">
+                    Login
+                </Button>
+            </Form>
+        </div>
+
+        <Footer/>
+    </>)
+}
 export default LoginPage;
