82ecb4762d05c6bc206d6e826e4692c6d5611da0
Mauricio Mendez <emauriciomendez@gmail.com>
2023-05-04 19:53:51 -0500

    primer commit de contact

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -9,6 +9,22 @@ function Contact() {
         <div>
             <button onClick={() => navigate("/")}>Back to Home</button>
             <h1>Working in progres...</h1>
+            <div class="container">
+        {/* <NavBar /> */}
+        en ontact
+        <div class="border  container-fluid">
+        <div class="row">
+          <div class="col">
+            <h2>Contáctanos</h2>
+          </div>
+          <div class="col">
+          </div>
+          </div>
+          </div>
+
+            </div>
+
+
         </div>
     )
 }
