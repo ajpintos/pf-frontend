6fa8bc2d91a341f5134b5ef59855dee3a4bd515e
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-05 12:25:00 -0500

    trabajando en mi parte Santiago Diaz

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Contact.module.css";
+import phone from "./img/phone.png";
+import direccion from "./img/direccion.png";
+import email from "./img/email.jpg";
 import { useState } from "react";
 import validate from "./validate.js";
 
@@ -34,17 +37,17 @@ function Contact() {
   };
 
   return (
-    <div class='container'>
+    <div class="container">
       <button onClick={() => navigate("/")}>Back to Home</button>
 
       {/* <NavBar /> */}
 
-      <div >
+      <div>
         <div class="row  g-3 ">
-          <div class="col-md-6 col-xm-12 g-5 p-5 border" >
+          <div class="col-md-6 col-xm-12 g-5 p-5 border">
             <h2>Dejanos tu mensaje</h2>
-          
-            <div >
+
+            <div>
               <label htmlFor="name">Nombres y Apellidos</label>
               <input
                 class="form-control"
@@ -55,18 +58,15 @@ function Contact() {
                 onChange={handleChange}
                 aria-label="Nombre"
               />
-              <div>                
+              <div>
                 <span className={styles.error}>
                   {errors.name ? errors.name : null}
                 </span>
               </div>
             </div>
 
-
-   
             <div class="row  g-3">
               <div class="col-md-6 col-xm-12 p-3">
-             
                 <input
                   type="text"
                   name="email"
@@ -79,26 +79,26 @@ function Contact() {
                 <span className={styles.error}>
                   {errors.email ? errors.email : null}
                 </span>
-            </div>
-            <div class="col-md-6 col-xm-12 p-3">
-              
-              <input
-                type="text"
-                name="phone"
-                class="form-control"
-                value={form.phone}
-                maxlength="20"
-                onChange={handleChange}
-                placeholder="Celular" aria-label="Celular"
-              />
-              <div>
-                <span className={styles.error}>
-                  {errors.phone ? errors.phone : null}
-                </span>
+              </div>
+              <div class="col-md-6 col-xm-12 p-3">
+                <input
+                  type="text"
+                  name="phone"
+                  class="form-control"
+                  value={form.phone}
+                  maxlength="20"
+                  onChange={handleChange}
+                  placeholder="Celular"
+                  aria-label="Celular"
+                />
+                <div>
+                  <span className={styles.error}>
+                    {errors.phone ? errors.phone : null}
+                  </span>
+                </div>
               </div>
             </div>
-            </div>
-            <div >
+            <div>
               Mensaje
               <textarea
                 class="form-control"
@@ -106,36 +106,68 @@ function Contact() {
                 onChange={handleChange}
                 name="message"
               ></textarea>
-               <div class='row p-3'>
+              <div class="row p-3">
                 <span className={styles.error}>
                   {errors.message ? errors.message : null}
                 </span>
               </div>
             </div>
             {/* <div class='row p-3'> */}
-            <button class='btn btn-primary' > Enviar</button>
+            <button class="btn btn-primary"> Enviar</button>
             {/* </div> */}
           </div>
 
-          <div class="col-md-6 col-xm-12 g-3" >
-            <div class="row border border-danger">
-             
-              <h3>BioFresh</h3>
-              <div>
-                Si tienes alguna duda sobre algún producto. quieres contarnos
-                sobre un evento o simplemente quieres escribirnos, déianos tu
-                mensaje aquí!
-              </div>
+          <div class="col border border-info border-1">
+            <div class="container col border border-primary border-1 mt-5">
+              <div class="row border border- border-1">
+                <h5 class="col-md-6 text-start border border-secondary border-1">
+                  BioFresh
+                </h5>
+                <div class="col-m-4 border border-secondary border-1 text-start small ">
+                  Si tienes alguna duda sobre algún producto. quieres contarnos
+                  sobre un evento o simplemente quieres escribirnos, déianos tu
+                  mensaje aquí!
+                </div>
 
-              <h2>Contáctanos</h2>
-              <div class="row  g-3">
-              <h3 class="col border border-primary">Telefono : </h3>
-              <div class="col border border-primary"> 3007476099 </div>
-             </div>
-              <h3 class="col border border-secondary">Direccion</h3>
-              <div class="col border border-danger">Carrera 33 # ejemplo</div>
-              <h3 class="row">Email</h3>
-              <div class="row">Biofresh@gmail.com</div>
+                <div class="row text-white">" "</div>
+                <h5 className="col-md-4 border border-secondary border-1 border">
+                  Contáctanos
+                </h5>
+                <br />
+                <div class="d-flex flex-wrap  border border-primary border-1 border">
+                  <div class="row text-white ">" "</div>
+
+                  <h6 class="col border border-danger border-1 border">
+                    <img src={phone} alt="" width="40px" height="40px" />
+                    <div>
+                      Telefono
+                      <br />
+                      <h7 /* class="col border border-secondary border-3 border" */
+                      >
+                        3007476099
+                      </h7>
+                    </div>
+                  </h6>
+
+                  <h6 class="col-md-6 border border-secondary border-1 border">
+                    <img src={direccion} alt="" width="40px" height="40px" />
+                    Direccion
+                    <br />
+                    <h7 class="col border border-secondary border-1 border">
+                      Carrera 33 # ejemplo
+                    </h7>
+                  </h6>
+
+                  <h6 class="col-md-6 border border-secondary border-1 border">
+                    <img src={email} alt="" width="40px" height="40px" />
+                    Email
+                    <br />
+                    <h7 class="col border border-secondary border-1 border">
+                      Biofresh@gmail.com
+                    </h7>
+                  </h6>
+                </div>
+              </div>
             </div>
           </div>
           <br />
