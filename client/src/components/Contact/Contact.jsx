4361a018bc50a14cd8f7908636f1899e3af156a7
Mauricio Mendez <emauriciomendez@gmail.com>
2023-05-04 20:08:03 -0500

    segundo  commit de contact

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -6,26 +6,46 @@ function Contact() {
     const navigate = useNavigate();
 
     return (
-        <div>
+        <div class="container">
             <button onClick={() => navigate("/")}>Back to Home</button>
-            <h1>Working in progres...</h1>
-            <div class="container">
+            
+           
         {/* <NavBar /> */}
-        en ontact
+        
         <div class="border  container-fluid">
         <div class="row">
           <div class="col">
-            <h2>Contáctanos</h2>
-          </div>
-          <div class="col">
-          </div>
-          </div>
+            <h2>Dejanos tu mensaje</h2>
+            <div>Nombres y Apellidos 
+                <input type="text" name='name'></input>
+            </div>
+            <div>Email
+                <input type="text" name='email'></input>
+            </div>
+            <div>Celular
+                <input type="text" name='phone'></input>
+            </div> 
+            <div>Mensaje
+                <input type="text" name='mensaje'></input>
+            </div>
+           <button> Enviar</button>
           </div>
 
+          <div class="col">
+                <h2>Contáctanos</h2>
+          </div>
+          </div>
+          </div>
+          <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+        crossorigin="anonymous"
+      ></link>
             </div>
 
 
-        </div>
+       
     )
 }
 
