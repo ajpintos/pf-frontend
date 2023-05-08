41e4c09d61c0f10b185272f85115aa37e1574e93
ajpintos <ajpintos@gmail.com>
2023-05-07 23:28:54 -0300

    arreglado el lio del merge, bajen esta versión que es la última sana

--- client/src/Redux/types.js
+++
@@ -1,2 +0,0 @@
-export const ADD_FAVORITES = "ADD_FAVORITES";
-export const DELETE_FAVORITES = "DELETE_FAVORITES";
--- client/src/components/LandingHome/LandingHome.jsx
+++ client/src/components/LandingHome/LandingHome.jsx
@@ -1,50 +1,87 @@
-import s from "./Landing_home.module.css";
-import imgpropia from "../../logo/logo.png";
-import { Link } from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 import CardContainer from "../CardContainer/CardContainer.jsx";
 import Footer from "../Footer/Footer.jsx";
 import NavBar from "../NavBar/NavBar";
+import Settings from "../Settings/Settings";
+import SearchBar from "../SearchBar/SearchBar";
+import {getProducts} from "../../Redux/actions/actionsProducts";
+import {getCategories} from "../../Redux/actions/actionsCategories";
+import imgpropia from "../../logo/logo.png";
+import s from "./Landing_home.module.css";
+import Stack from "react-bootstrap/esm/Stack.js";
+import { useSelector } from "react-redux";
 
 const Landing_home = () => {
-  return (
-    <div className={s.container}>
-      {/* Cabecera */}
-      <header>
-        {/* Sección del logo, login, favoritos y carrito */}
-        <div className={s.container_inicio}>
-          <img src={imgpropia} alt="imagen" className={s.logo} />
-          <input type="text" placeholder="Search..." className={s.search} />
-          <button className={s.my_acount}>
-            <Link to="/login">🙋‍♂️My account</Link>
-          </button>
-          <Link to={"/favorites"}>
-            <button className={s.whishlist}>🧡Whishlist</button>
-          </Link>
-          <button className={s.amount}>🛒AMOUNT</button>
-          <br />
-          <br />
+
+    const dispatch = useDispatch();
+
+    const [flagChange, setFlagChange] = useState(false);
+
+    const changeFlag = (flag) => {
+        setFlagChange(flag);
+    }
+
+    const loadingData = async () => {
+        const all_Products = await getProducts();
+        dispatch(all_Products);
+        const all_Categories = await getCategories();
+        dispatch(all_Categories);
+    };
+
+    useEffect(() => {
+        loadingData();
+    }, []);
+
+    // const user = {name: "Javier"}
+    const user = useSelector(state => state.userLogin);
+
+    return (
+        <div className="container-fluid">
+            {/* Cabecera */}
+            <header>
+                {/* Sección del logo, login, favoritos y carrito */}
+                <div className="row justtify-content-center align-items-center">
+                    <figure className='col-6 col-sm-5 col-md-4 col-lg-3'>
+                        <img src={imgpropia} alt="Biofresh Logo" className='img-fluid w-50 p-0'/>
+                    </figure>
+
+                    {user.email ? 
+                        <Link to="/myaccount" className="col-2 col-sm-1 offset-sm-4 col-md-1 offset-md-5 col-lg-1 offset-lg-6">
+                          🙋‍♂️ {user.firstname}
+                        </Link> 
+                        :
+                        <Link to="/login" className="col-2 col-sm-1 offset-sm-4 col-md-1 offset-md-5 col-lg-1 offset-lg-6">
+                          🙋‍♂️ Login
+                        </Link>
+                    }
+                    <Link to='/' className="col-2 col-sm-1 col-md-1 col-lg-1">🧡 Fav</Link>
+                    <Link to='/' className="col-2 col-sm-1 col-md-1 col-lg-1">🛒 Cart</Link>
+                </div>
+
+                {/* Sección de NavBar y Settings */}
+
+                <Stack direction="horizontal" className="d-flex flex-row justify-content-between bg-success pt-3 pb-3">
+                    <NavBar/>
+                    <Settings/>
+                </Stack>
+
+
+                {/* Sección Hero */}
+                <div className={s.hero}>
+                    <h1 className={s.text}>The Best and Healthiest you find here</h1>
+                </div>
+            </header>
+
+            {/* Sección Cards */}
+            <SearchBar/>
+            <CardContainer flagChange={flagChange} changeFlag={changeFlag}/>
+
+            {/* Footer */}
+            <Footer/>
         </div>
-
-        {/* Sección de NavBar y Settings */}
-
-        <NavBar />
-
-        {/* Sección Hero */}
-        <div className={s.hero}>
-          <h1 className={s.text}>HERO</h1>
-        </div>
-      </header>
-
-      {/* Sección Cards */}
-      <section>
-        <h2 className={s.feactured_products}>FEATURED PRODUCTS</h2>
-        <CardContainer />
-      </section>
-
-      {/* Footer */}
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default Landing_home;
