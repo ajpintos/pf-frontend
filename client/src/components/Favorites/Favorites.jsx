7d9f30125f7f805d3b1085b210f69f26fede3654
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-09 01:25:07 -0500

    stylo a favorites

--- client/src/components/Favorites/Favorites.jsx
+++ client/src/components/Favorites/Favorites.jsx
@@ -11,24 +11,35 @@ const Favorites = () => {
   console.log(favorite);
 
   return (
-    <div className="container-fluid border border-info border-3">
+    <div className="container-fluid text-center border border-info border-3 vh-100 vw-100">
       <h1>Favorites en Desarrollo</h1>
-      {favorite.map((e) => {
-        return (
-          <div>
-            <div className="">
-              <Link to="/">
-                <button>Volver</button>
-              </Link>
-              <div className="">NOMBRE:{e.name}</div>
-              <div>DESCRIPTION:{e.description}</div>
-              <img src={e.image} alt="" width="100%" height="100px" />
-              <div>PRECIO:{e.price}</div>
-              <div>DISPONIBLES:{e.stock}</div>
-            </div>
-          </div>
-        );
-      })}
+      <Link to="/">
+        <button>Volver</button>
+      </Link>
+      <div className="border border-danger border-3 ">
+        <div
+          style={{
+            width: "200px",
+            height: "200px",
+            display: "flex",
+            margin: "5px",
+          }}
+        >
+          {favorite.map((e) => {
+            return (
+              <div className="border border-success border-2">
+                <div>
+                  <div>NOMBRE:{e.name}</div>
+                  <div>DESCRIPTION:{e.description}</div>
+                  <img src={e.image} alt="" width="20%" height="20px" />
+                  <div>PRECIO:{e.price}</div>
+                  <div>DISPONIBLES:{e.stock}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
