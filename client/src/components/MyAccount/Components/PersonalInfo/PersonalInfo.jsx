5227449f26c375f7887e2876523ffe8c65e978e2
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-11 01:02:44 -0500

    mejorando la interfaz

--- client/src/components/MyAccount/Components/PersonalInfo/PersonalInfo.jsx
+++ client/src/components/MyAccount/Components/PersonalInfo/PersonalInfo.jsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import { allUsers } from "../../../../Redux/actions/actionsUserLogin";
 import { useDispatch } from "react-redux";
+import prueba from "./img/yoda-bebe-6617.jpg";
+import { Link } from "react-router-dom";
 
 const PersonalInfo = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(allUsers());
-  }, []);
-
-  /*   const user = useState((state) => state.users); */
-  console.log();
-
   return (
     <div className=" text-center  ">
       <div className="">
-        <p className="mt-5">persona 1</p>
-        <img src={"./img/1.png"} alt="" width={"100px"} height={"100px"} />
+        <p className="mt-2">persona 1</p>
+        <img
+          src={prueba}
+          alt=""
+          width={"120px"}
+          height={"120px"}
+          style={{ borderRadius: "5rem" }}
+        />
+        <br />
         <br />
         <p>John Doe</p>
         <p>john.doe@gmail.com</p>
@@ -27,32 +28,41 @@ const PersonalInfo = () => {
             <div className="col text-start">🚌</div>
           </div>
           <br />
+
+          <Link to="/form_update">
+            <button
+              style={{
+                borderRadius: "3rem",
+                backgroundColor: "white",
+                color: "black",
+                border: "1px solid blue",
+              }}
+            >
+              administrar datos de Biofresh
+            </button>
+          </Link>
           <div className=" ">
             <button
               style={{
-                border: "2px solid blue",
+                border: "1px solid blue",
                 backgroundColor: "white",
                 color: "black",
                 borderRadius: "2rem",
+                marginTop: "20px",
               }}
             >
               Administra tu cuenta de google
             </button>
           </div>
-          <br />
-          <div className="mt-3 text-end">otras configuraciones</div>
-          <br />
-          <button style={{ borderRadius: "1rem" }}>
-            Cambiar datos personales de la base de datos de Biofresh?
-          </button>
 
-          <div>cambiar First Name: ejemplo 1</div>
+          {/*  <div>cambiar First Name: ejemplo 1</div>
           <div>cambiar Last Name: ejeplo 2</div>
           <div>cambiar Address: 123121</div>
           <div>Zip Code: 123123</div>
           <div>Country: 12312</div>
           <div>Phone: 3431212</div>
-          <div>City: prueba</div>
+          <div>City: prueba</div> */}
+          <br />
           <br />
         </div>
       </div>
