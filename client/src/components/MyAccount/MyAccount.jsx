fe870477f19b7f1f790fea95931e160a8b7b349a
santimuller45 <santiagomuller45@gmail.com>
2023-05-15 20:10:04 -0300

    added in myAcc navigate

--- client/src/components/MyAccount/MyAccount.jsx
+++ client/src/components/MyAccount/MyAccount.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 
 const MyAccount = () => {
 
+    const navigate = useNavigate();
     const user = useSelector(state => state.userLogin);
+
+    useEffect(() => {
+        !user.email && navigate('/');
+    }, [user]);
 
     return (
         <div>
