e3f9937915abce69887be052a5ab2f383a3cb427
santimuller45 <santiagomuller45@gmail.com>
2023-05-18 20:59:04 -0300

    nueva rama

--- client/src/components/Title/Title.jsx
+++ client/src/components/Title/Title.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Link , useNavigate } from "react-router-dom";
 import imgpropia from "../../logo/logo.png";
 import { useSelector , useDispatch } from 'react-redux';
