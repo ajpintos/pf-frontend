f89ed7c8b6f4f8026fca3e25aa26d3ff4cecc280
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-19 19:03:13 -0500

    avances

--- client/src/components/Settings/CategoriesSettings/AddCategorie.jsx
+++ client/src/components/Settings/CategoriesSettings/AddCategorie.jsx
@@ -1,25 +1,35 @@
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
+import { useState } from "react";
 
 const AddCategorie = () => {
+  const [show, setShow] = useState(true);
+  const handleClose = () => setShow(false);
+
+  const [form, setForm] = useState({});
+
   return (
-    
-      <Modal  >
-        <Modal.Header>Add Categorie</Modal.Header>
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <h3>Add Categorie</h3>
+        </Modal.Header>
         <Modal.Body>
           <Form>
             <Form.Group>
               <Form.Control placeholder="name" />
             </Form.Group>
+            <br />
             <Form.Group>
-              <Form.Control placeholder="description" />
+              <Form.Control as="textarea" rows={5} placeholder="description" />
             </Form.Group>
-       
+            <br />
+            <Button>add</Button>
           </Form>
         </Modal.Body>
       </Modal>
-    
+    </>
   );
 };
 export default AddCategorie;
