ef88df9f07b9f848f0ca5e8b3f1e17633f990523
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-05-20 14:16:44 -0500

    avanves

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -6,6 +6,7 @@ import Footer from "../Footer/Footer";
 import UsersSettings from "../UsersSettings/SettingsGeneral/UsersSettings";
 import ProductsSettings from "../ProductsSettings/ProductsSettings";
 import CategoresSettings from "../Settings/CategoriesSettings/CategoriesSettingGeneral";
+import { useSelector } from "react-redux";
 
 function Settings() {
   const [show, setShow] = useState({
@@ -24,6 +25,10 @@ function Settings() {
     setShow({ users: false, products: false, categories: true });
   };
 
+  const user = useSelector((state) => state.userLogin);
+
+  console.log(user);
+
   return (
     <div className="container-fluid">
       <Title />
@@ -35,21 +40,27 @@ function Settings() {
       </Stack>
 
       <div className="text-center mt-5 mb-5">
-        <h4>administrator options</h4>
-        <button onClick={handlerShowU} style={{ borderRadius: "2rem" }}>
-          Users
-        </button>
-        <></> <></>
-        <button onClick={handlerShowP} style={{ borderRadius: "2rem" }}>
-          Products
-        </button>
-        <></> <></>
-        <button onClick={handlerShowC} style={{ borderRadius: "2rem" }}>
-          Categories
-        </button>
-        {show.users ? <UsersSettings /> : null}
-        {show.products ? <ProductsSettings /> : null}
-        {show.categories ? <CategoresSettings /> : null}
+        {user.email ? (
+          <div>
+            <h4>administrator options</h4>
+            <button onClick={handlerShowU} style={{ borderRadius: "2rem" }}>
+              Users
+            </button>
+            <></> <></>
+            <button onClick={handlerShowP} style={{ borderRadius: "2rem" }}>
+              Products
+            </button>
+            <></> <></>
+            <button onClick={handlerShowC} style={{ borderRadius: "2rem" }}>
+              Categories
+            </button>
+            {show.users ? <UsersSettings /> : null}
+            {show.products ? <ProductsSettings /> : null}
+            {show.categories ? <CategoresSettings /> : null}
+          </div>
+        ) : (
+          <h1>Setting Normales</h1>
+        )}
       </div>
       <Footer />
     </div>
