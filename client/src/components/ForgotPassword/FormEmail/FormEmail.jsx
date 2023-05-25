058e2998ce41014c7c35825ac23bc548fd2a47e8
Mauricio Mendez <emauriciomendez@gmail.com>
2023-05-24 23:53:07 -0500

    ForgotPassword/ Se implemento email 80/100

--- client/src/components/ForgotPassword/FormEmail/FormEmail.jsx
+++ client/src/components/ForgotPassword/FormEmail/FormEmail.jsx
@@ -13,13 +13,15 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import registerValidate from "../../RegisterPage/validate/registerValidate.js";
+import swal from 'sweetalert';
+import emailjs from "@emailjs/browser";
 
 const FormEmail = () => {
 
     const [ email , setEmail ] = useState("")
     const navigate = useNavigate();
     const dispatch = useDispatch();
-
+    const Dom = document.getElementById("formToSend");
     const [form, setForm] = useState({
         email: ""
     })
@@ -39,9 +41,28 @@ const FormEmail = () => {
     const handlerSubmitEmail = (e) => {
         e.preventDefault();
         dispatch(emailForgotPassword(email));
+        emailjsSend();
         navigate('/forgotpassword/changepassword');
     }
-
+const emailjsSend=()=>{
+    const serviceID = "service_e5hd1wt";
+      const templateID = "template_8i1yknj";
+      const key_public = "gEu_FBDo_Q0lvhmwA";
+     emailjs.sendForm(serviceID, templateID, Dom, key_public)
+        .then(
+        (result) => {
+          console.log(result.text);
+         // setSuccessMessage("Message sent succesfully");
+          swal("Ok!", "Message sent succesfully!", "success");
+         
+        },
+        (error) => {
+          console.log(error.text);
+        //  setErrorMessage("Error sending the message");
+          swal("Error!", "No  sending the message", "error")
+        }
+      );
+}
 
     return (
         <div className="container-fluid">
@@ -59,6 +80,7 @@ const FormEmail = () => {
                                 onChange={handleChangeEmail}
                             />
                         </Form.Group>
+                        <input type="text" name='token' value='token_por_definir' hidden='true'/>
                     <p style={{color: "red"}}>{errors.email}</p>
                         <Button variant="success" type="submit" size='sm'>Submit</Button>
                 </Form> 
